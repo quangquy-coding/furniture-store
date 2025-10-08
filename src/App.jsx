ce8e7f01b49a0b4b3521af19535e22a4a462642b
Nguyễn Quang Quý <quangquy822003@gmail.com>
2025-10-08 17:38:42 +0700

    push all

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-blue-500 text-white text-center p-4 rounded-lg">
+      TailwindCSS đang hoạt động! 🎨
+    </div>
+  );
+};
+
+export default App;
